1c3f26b86e2dcbeca4704580bc1ddac127a5c08b
akzholbek <akzholbek0096@gmail.com>
2024-02-26 23:00:54 +0000

    first commit

---
+++ src/test/java/com/neobis/neoauth/NeoauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobis.neoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
