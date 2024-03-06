887f2ea555350aac631019c20318cf2e6ecdd15b
akzholbek <akzholbek0096@gmail.com>
2024-03-06 13:15:57 +0000

    added scheduled task to send confirmation email to users which are not enabled

--- src/main/java/com/neobis/neoauth/repository/UserRepository.java
+++ src/main/java/com/neobis/neoauth/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -16,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailOrUsername(@Param("emailOrUsername") String emailOrUsername);
 
 
+    @Query("Select u From User u Where u.isEnabled = false")
+    List<User> findNotEnabledUsers();
 }
--- src/main/java/com/neobis/neoauth/service/Impl/UserServiceImpl.java
+++ src/main/java/com/neobis/neoauth/service/Impl/UserServiceImpl.java
@@ -16,6 +16,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
@@ -28,6 +29,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -258,5 +260,20 @@ public class UserServiceImpl implements UserService {
                 null,
                 user);
         return confirmationToken;
+    }
+
+
+
+    @Scheduled(cron = "0 0 12 * * MON")
+    private void sendWeeklyConfirmEmail() {
+        List<User> users = userRepository.findNotEnabledUsers();
+        for(User user: users){
+            ConfirmationToken confirmationToken = generateConfirmToken(user);
+            confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+            String link = CONFIRM_EMAIL_LINK + confirmationToken.getToken();
+            sendConfirmationMail(link, user);
+        }
+
     }
 }
